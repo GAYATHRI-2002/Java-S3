3c15fb4ff099c12c1182089b955daa66522bc48f
GAYATHRI-2002 <83536175+GAYATHRI-2002@users.noreply.github.com>
2022-01-23 17:39:57 +0530

    Create factoial.java

---
+++ Cycle1/factoial.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class factorial
+{
+   public static void main (String args[])
+   {
+     Scanner obj= new Scanner(System.in);
+     int ,fact=1;
+     System.out.println("enter the number");
+     n=obj.nextInt();
+     for(i=0;i<=n;i++)
+       fact=fact*i;
+   }
+}
